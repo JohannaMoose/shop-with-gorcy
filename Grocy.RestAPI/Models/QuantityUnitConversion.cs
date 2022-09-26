e7ec97b656d4dc683c21fee95785fb5af1cdb48e
Johanna Hultén <min.mail@johannap.se>
2022-09-26 12:30:04 +0200

    Added quantity conversion

---
+++ Grocy.RestAPI/Models/QuantityUnitConversion.cs
@@ -0,0 +1,3 @@
+﻿namespace Grocy.RestAPI.Models;
+
+public record QuantityUnitConversion(string Id, string From_qu_id, string To_qu_id, string Factor, string Product_id, string Row_created_timestamp);
---
+++ Grocy.RestAPI/QuantityConversionApi.cs
@@ -0,0 +1,12 @@
+﻿using Grocy.RestAPI.Models;
+
+namespace Grocy.RestAPI;
+
+public class QuantityConversionApi : GrocyApiBase<QuantityUnitConversion>
+{
+    public QuantityConversionApi(HttpClient httpClient, string grocyUrl) : base(httpClient, grocyUrl)
+    {
+    }
+
+    protected override string ApiEndpoint => "quantity_unit_conversions";
+}
