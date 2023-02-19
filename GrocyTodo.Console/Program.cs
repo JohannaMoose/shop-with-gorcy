7b81e37089490fc2f7135679437a4b82518e3c09
Johanna Hultén <min.mail@johannap.se>
2023-02-19 11:15:10 +0100

    Added ToDo Console app

---
+++ GrocyTodo.Console/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Grocy ToDo World!");
+var grocyInstance = Environment.GetEnvironmentVariable("GROCY_INSTANCE");
