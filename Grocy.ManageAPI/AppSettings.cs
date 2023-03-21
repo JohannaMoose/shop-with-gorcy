37242a3c571e31a2cb11e1946f11706c948b9563
Johanna Hultén <min.mail@johannap.se>
2023-03-21 14:09:03 +0100

    Setup of controller

---
+++ Grocy.ManageAPI/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace Grocy.ManageAPI;
+
+public class AppSettings
+{
+    public string GrocyInstanceUrl
+    {
+        get
+        {
+            return "";
+        }
+    }
+
+    public string GrocyApiKey
+    {
+        get
+        {
+            return "";
+        }
+    }
+}
---
+++ Grocy.ManageAPI/Controllers/ChoresController.cs
@@ -0,0 +1,26 @@
+﻿using Grocy.ManageAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grocy.ManageAPI.Controllers
+{
+    [Route("api/chores")]
+    [ApiController]
+    public class ChoresController : ControllerBase
+    {
+        public ChoresController(CategoryChoreManager manager)
+        {
+            Manager = manager;
+        }
+
+        private CategoryChoreManager Manager { get; set; }
+
+        [HttpPost("schedule")]
+        public async Task<IActionResult> ScheduleChores()
+        {
+            await Manager.ScheduleCategory("Städning", 1);
+            await Manager.ScheduleCategory("Tvätt", 2);
+
+            return Ok();
+        }
+    }
+}
